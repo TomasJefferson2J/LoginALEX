df99fab075ecd644ae24cb8a973c52d2e5051fbf
AlexanderdpS <115846092+AlexanderdpS@users.noreply.github.com>
2022-10-14 18:36:13 -0500

    Add files via upload

---
+++ consultasufragio/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.TextNombre = New System.Windows.Forms.TextBox()
+        Me.TextDNI = New System.Windows.Forms.TextBox()
+        Me.TextEdad = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(10, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(565, 179)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(36, 218)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(316, 250)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'TextNombre
+        '
+        Me.TextNombre.Location = New System.Drawing.Point(46, 284)
+        Me.TextNombre.Name = "TextNombre"
+        Me.TextNombre.Size = New System.Drawing.Size(268, 23)
+        Me.TextNombre.TabIndex = 2
+        '
+        'TextDNI
+        '
+        Me.TextDNI.Location = New System.Drawing.Point(46, 381)
+        Me.TextDNI.MaxLength = 8
+        Me.TextDNI.Name = "TextDNI"
+        Me.TextDNI.Size = New System.Drawing.Size(268, 23)
+        Me.TextDNI.TabIndex = 3
+        '
+        'TextEdad
+        '
+        Me.TextEdad.Location = New System.Drawing.Point(46, 471)
+        Me.TextEdad.Name = "TextEdad"
+        Me.TextEdad.Size = New System.Drawing.Size(268, 23)
+        Me.TextEdad.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(381, 296)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(155, 152)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "CONSULTAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(585, 535)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextEdad)
+        Me.Controls.Add(Me.TextDNI)
+        Me.Controls.Add(Me.TextNombre)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents TextNombre As TextBox
+    Friend WithEvents TextDNI As TextBox
+    Friend WithEvents TextEdad As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ consultasufragio/Form1.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Security.Principal
+
+Public Class Form1
+    Dim Edad As Double
+    Dim Consulta As Integer
+
+
+
+    Private Sub TextNombre_TextChanged(sender As Object, e As EventArgs) Handles TextNombre.TextChanged
+
+    End Sub
+
+    Private Sub TextDNI_TextChanged(sender As Object, e As EventArgs) Handles TextDNI.TextChanged
+
+    End Sub
+
+    Private Sub TextEdad_TextChanged(sender As Object, e As EventArgs) Handles TextEdad.TextChanged
+
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Operar()
+        If Edad >= 18 Then
+            MessageBox.Show("Cantidad de Sufragios: " & Consulta, "¡GRACIAS!")
+        ElseIf Edad < 18 Then
+            MessageBox.Show("¡Usted aun no tiene edad para sufragar!", "¡GRACIAS!")
+        End If
+    End Sub
+
+    Private Sub TextDNI_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextDNI.KeyPress
+        e.Handled = Not IsNumeric(e.KeyChar) And Not Char.IsControl(e.KeyChar)
+        If Not IsNumeric(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            MsgBox("¡Ingrese los 8 dígitos de su DNI!")
+        End If
+    End Sub
+
+    Private Sub TextEdad_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextEdad.KeyPress
+        e.Handled = Not IsNumeric(e.KeyChar) And Not Char.IsControl(e.KeyChar)
+        If Not IsNumeric(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            MsgBox("¡Ingrese su edad solo en números!")
+        End If
+    End Sub
+    Public Sub Operar()
+        Edad = Val(TextEdad.Text)
+        Consulta = ((Edad - 19) / 4) + 1
+    End Sub
+End Class
